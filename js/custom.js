06dd7c54b985835e15955db9454a688c239b7409
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-07-04 18:15:13 +0900

    project09
    project09

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
 $(function () {
+    $('button').click(function () {
+        var $this = $(this);
+        var index = $this.index();
+
+        $this.addClass('active');
+        $this.siblings('button.active').removeClass('active');
+
+        var $outer = $this.closest('.outer');
+        var $current = $outer.find(' > .tabs > .tab.active');
+        var $post = $outer.find(' > .tabs > .tab').eq(index);
+
+        $current.removeClass('active');
+        $post.addClass('active');
+        // 위의 코드는 탭메뉴 코드입니다.
+    });
 
 })
