717f68891641e52a53ec65b9dfe64ec83a4d7007
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-07-07 14:16:02 +0900

    project09
    project09

--- js/custom.js
+++ js/custom.js
@@ -14,4 +14,35 @@ $(function () {
         $post.addClass('active');
     });
 
-})
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+        $('h1').toggleClass('on');
+    });
+
+    // $('.price_wrap').slick({
+    //     arrows: false,
+    //     slidesToShow: 3,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //             }
+    //         },
+    //     ]
+    // });
+
+    // $('.tab .book_shelf').slick({
+    //     arrows: false,
+    //     slidesToShow: 7,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //             }
+    //         },
+    //     ]
+    // });실패....ㅠㅠ 
+});
